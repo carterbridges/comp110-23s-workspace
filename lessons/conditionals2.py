a252850768238f1c44b935998cae7cc441251354
Carter Bridges <cartersb@ad.unc.edu>
2023-01-20 10:18:19 -0500

    input.py

---
+++ lessons/conditionals2.py
@@ -0,0 +1,16 @@
+"""Knock knock joke with conditionals"""
+
+inter_cow: str = input("Do you want an interrupting cow?")
+
+
+if (inter_cow == "yes"):
+    print("Knock Knock")
+    print("... who's there?" )
+    print("interrupting cow" )
+    print("Interrupting cow who---" )
+    print("---MOO!!" )
+
+else: 
+    print("Oh ... okay... :(")
+    inter_cow == ("you're not funny")
+    
